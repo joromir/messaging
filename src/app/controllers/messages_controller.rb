b7b7fe2f0a6fa7e158e6bccb34bf26c2ca960629
Georgi Kostov <joromir@protonmail.com>
2017-06-04 16:37:06 +0300

    generate messages controller

---
+++ src/app/controllers/messages_controller.rb
@@ -0,0 +1,29 @@
+class MessagesController < ApplicationController
+  def index
+    render plain: 'TODO: index'
+  end
+
+  def show
+    render plain: 'TODO: show'
+  end
+
+  def new
+    render plain: 'TODO: new'
+  end
+
+  def create
+    render plain: 'TODO: create'
+  end
+
+  def edit
+    render plain: 'TODO: edit'
+  end
+
+  def update
+    render plain: 'TODO: update'
+  end
+
+  def destroy
+    render plain: 'TODO: destroy'
+  end
+end
